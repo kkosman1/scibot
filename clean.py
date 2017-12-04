20871bdad64048093fbed059790a834fb04a5383
Kimberly Kosman <kkosman1@nd.edu>
2017-12-04 16:14:27 -0500

    change clean

--- clean.py
+++ clean.py
@@ -3,22 +3,28 @@ import numpy as np
 
 def clean():
 	df = pd.read_csv('index.txt', sep="\t", index_col='pid', names=['folder', 'pdfid', 'pid', 'title'])
+	df['pid'] = df['pid'].astype(str)
 	
 	#read in papers to df and drop unnecessary columns
 	papers = pd.read_csv('Papers.txt', sep="\t", names=['pid', 'title_case', 'title', 'year', 'date_of_proceeding', 'doi', 'conf_full_name', 'drop1', 'cid', 'drop2'])
 	for col in ['title', 'date_of_proceeding', 'doi', 'conf_full_name', 'drop1', 'drop2']:
 		del papers[col]
+	papers['pid'] = papers['pid'].astype(str)
 
 	#read in paper keywords to df
 	keywords = pd.read_csv('PaperKeywords.txt', sep="\t", names=['pid', 'keyword', 'kid'])
 	del keywords['kid']
-	
+	keywords['pid'] = keywords['pid'].astype(str)	
+
 	#read in paper author affiliations to df
 	affiliation = pd.read_csv('PaperAuthorAffiliations.txt', sep="\t", names=['pid', 'aid', 'fid', 'aff_org', 'aff', 'sid'])
 	del affiliation['aff_org']
+	affiliation['pid'] = affiliation['pid'].astype(str)
+	affiliation['aid'] = affiliation['aid'].astype(str)
 
 	#read in authors to df
 	authors = pd.read_csv('Authors.txt', sep="\t", names=['aid', 'aut'])
+	authors['aid'] = authors['aid'].astype(str)
 
 	print("FIRST ONE")
 	print(df.head())
